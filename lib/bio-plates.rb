b0c9bd56bbe33f8f32bf3bb1541d152f5aebc96f
stveep <spettitt@gmail.com>
2016-01-29 17:36:58 +0000

    Adding plate annotation, RinRuby

--- lib/bio-plates.rb
+++ lib/bio-plates.rb
@@ -2,11 +2,11 @@
 # bioruby directory tree.
 #
 # For example, say you have a plugin named bio-plugin, the only uncommented
-# line in this file would be 
+# line in this file would be
 #
 #   require 'bio/bio-plugin/plugin'
 #
 # In this file only require other files. Avoid other source code.
 
 require 'bio-plates/plates.rb'
-
+require 'bio-plates/annotator.rb'
---
+++ lib/bio-plates/annotator.rb
@@ -0,0 +1,24 @@
+# Annnotation of plates given ranges
+# which I was too lazy to rewrite in Ruby.
+class BioPlates::Annotator
+  require 'rinruby'
+
+  class << self
+    attr_accessor :annotate_lib
+  end
+  @annotate_lib = File.dirname(__FILE__) + "/plate_annotate.r"
+
+  def self.read (file)
+    R.annofile = file
+    R.eval "read.table(annofile,head=T)"
+  end
+
+  def self.annotate (file,outfile)
+    BioPlates::Annotator.read(file)
+    R.eval "source(#{@annotate_lib})"
+    R.eval "anno_df <- annotate_plate(annofile)"
+    R.outfile = outfile
+    R.eval "write.csv(anno_df,outfile,quote=F,row.names=F)"
+  end
+
+end
